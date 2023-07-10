2228f25264cede3dffcd0398f87523f1d47c04d8
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 18:35:52 +0200

    using pic as background

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,8 +10,24 @@ export default {
 </script>
 
 <template>
-    <NavbarHeader />
-    <JumbotronHeader />
+    <section id="header">
+        <div class="container">
+            <NavbarHeader />
+            <JumbotronHeader />
+        </div>
+    </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/scss/var' as *;
+
+#header {
+    background-color: $c-g3;
+    background-image: url(../assets/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg);
+}
+
+.container {
+    width: 1200px;
+    margin: 0 auto;
+}
+</style>
