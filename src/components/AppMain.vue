0f93c5e6ab5fa7e7a9a2d9844e8c13e833dc5d1e
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 16:39:21 +0200

    main section structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,6 +5,14 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <section id="presentation"></section>
+    <section id="about-us"></section>
+    <section id="a-card"></section>
+    <section id="project"></section>
+    <section id="testimonial"></section>
+    <section id="blog"></section>
+    <section id="get-started"></section>
+</template>
 
 <style></style>
