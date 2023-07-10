12b3f3f60a27d3f031d004551e31109c4e3c282c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 18:32:37 +0200

    data update

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -10,3 +10,29 @@ const NavLink = [
 ];
 
 export { NavLink };
+
+// Presentation cards
+const PresCard = [
+  {
+    title: "STATISTICAL CONSULTING",
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a",
+    img: "https://demo.phlox.pro/consulting-2/wp-content/uploads/sites/113//2019/06/line-graph.svg",
+  },
+  {
+    title: "DIGITAL CONSULTING",
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a",
+    img: "https://demo.phlox.pro/consulting-2/wp-content/uploads/sites/113/2019/06/shout.svg",
+  },
+  {
+    title: "BANKING CONSULTING",
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a",
+    img: "https://demo.phlox.pro/consulting-2/wp-content/uploads/sites/113//2019/06/Path-13.svg",
+  },
+  {
+    title: "ENTERPRISE CONSULTING",
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a",
+    img: "https://demo.phlox.pro/consulting-2/wp-content/uploads/sites/113/2019/06/target.svg",
+  },
+];
+
+export { PresCard };
