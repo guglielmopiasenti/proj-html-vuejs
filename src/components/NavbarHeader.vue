efdf0083842189d24f40a9ebf846138d59dc259f
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 15:50:30 +0200

    uploading pictures

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -1,10 +1,24 @@
 <script>
+import { NavLink } from '../assets/data/data';
 export default {
-
+    data() {
+        return {
+            navLinks: NavLink
+        };
+    }
 }
 
 </script>
 
-<template></template>
+<template>
+    <img src="../assets/img/logotype-2-139x23.png" alt="">
+    <div>
+        <ul>
+            <li v-for="(navItem, index) in navLinks" :key="index">
+                {{ navItem.link }}
+            </li>
+        </ul>
+    </div>
+</template>
 
 <style lang="scss"></style>
