aac700bae2beabf5ba31a8b95b143ccbbd697446
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 18:34:47 +0200

    about us section

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+
+</script>
+<template>
+    <div>
+        <img src="../assets/img/business-people-working-together-on-project-and-5FHSKBL.jpg" alt="">
+        <div>
+            <div>ABOUT US</div>
+            <h1>WE MAKE YOUR STRATEGY WORK</h1>
+            <p> When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage
+                of my trees, and but a thousand unknown plants are noticed when I hear the buzz of the little world among
+                the stalks,and grow familiar indescribable forms of the insects .and flies, then I feel the presence of the
+                Almighty.
+            </p>
+        </div>
+    </div>
+</template>
+<style></style>
