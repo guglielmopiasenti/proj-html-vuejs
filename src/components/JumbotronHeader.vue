d3c37c9b1fc95d72978c1685da84e0c9fbe8a93b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 18:33:04 +0200

    presentation card section

--- src/components/JumbotronHeader.vue
+++ src/components/JumbotronHeader.vue
@@ -10,20 +10,11 @@ export default {
         <div>ALWAYS THE BEST WAY YOU NEED IT</div>
         <h1>THE BEST BUSINESS CONSULTING</h1>
         <a href="#" class="btn btn-a">READ MORE</a>
-        <img src="../assets/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg" alt="">
     </section>
 </template>
 
 <style lang="scss" scoped>
 #jumbotron {
     position: relative;
-}
-
-img {
-    position: absolute;
-    top: -170px;
-    right: 0px;
-    z-index: -1;
-    width: 1100px;
 }
 </style>
---
+++ src/components/PresentationCard.vue
@@ -0,0 +1,20 @@
+<script>
+import { PresCard } from '../assets/data/data';
+export default {
+    data() {
+        return {
+            presCard: PresCard,
+        };
+    }
+}
+</script>
+<template>
+    <div>
+        <div v-for="(card, index) in presCard" :key="index">
+            <img :src="card.img" alt="Card Image" />
+            <h3>{{ card.title }}</h3>
+            <p>{{ card.text }}</p>
+        </div>
+    </div>
+</template>
+<style></style>
