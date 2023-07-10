f119379c4a85cfa736c383a9c62f09e8bf5730d8
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 15:56:59 +0200

    nav link created

---
+++ src/assets/data/data.js
@@ -0,0 +1,12 @@
+// NAVBAR LINKS
+
+const NavLink = [
+  { link: "HOME" },
+  { link: "ABOUT" },
+  { link: "PROJECTS" },
+  { link: "SERVICES" },
+  { link: "BLOG" },
+  { link: "CONTACT" },
+];
+
+export { NavLink };
