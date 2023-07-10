b8f5e1635c5e86b1f594c52d8dd0e9b4a23ba848
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-10 14:19:48 +0200

    setting vue components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+
+}
 
 </script>
 
