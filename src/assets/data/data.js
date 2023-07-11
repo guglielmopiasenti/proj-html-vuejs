0ba0095f17d1b697cc33d74e1d7a0282a0e81282
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 08:24:04 +0200

    updating data

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -36,3 +36,50 @@ const PresCard = [
 ];
 
 export { PresCard };
+
+// project section
+const ProjectCard = [
+  {
+    img: "src/assets/img/DRY-1-790x592.jpg",
+    title: "Purinky Project",
+    category: "Uncategorized",
+    zoom: "+",
+  },
+  {
+    img: "src/assets/img/221bf0b7-8134-43bb-936a-5acbe42db64a-790x592.jpg",
+    title: "Purinky Project",
+    category: "Uncategorized",
+    zoom: "+",
+  },
+  {
+    img: "src/assets/img/z1el4c4p-790x592.jpg",
+    title: "Purinky Project",
+    category: "Uncategorized",
+    zoom: "+",
+  },
+];
+
+export { ProjectCard };
+
+// testimonial section
+const TestimonialCard = [
+  {
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed by when I hear the buzz of the little .",
+    author: "Vera Duncan",
+    company: "Amazon Inc.",
+    img: "src/assets/img/oliver-ragfelt-488196-2.jpg",
+  },
+  {
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed by when I hear the buzz of the little .",
+    author: "Vera Duncan",
+    company: "Amazon Inc.",
+    img: "src/assets/img/12679.jpg",
+  },
+  {
+    text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed by when I hear the buzz of the little .",
+    author: "Vera Duncan",
+    company: "Amazon Inc.",
+    img: "src/assets/img/oliver-ragfelt-488196-unsplash.jpg",
+  },
+];
+export { TestimonialCard };
