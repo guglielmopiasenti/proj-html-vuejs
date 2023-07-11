856487c3d3804480fad36dfc39b86f8362a3d8f8
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 15:59:06 +0200

    presentation style added

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -12,13 +12,42 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div class="card">
         <h1>{{ title }}</h1>
         <div v-for="(card, index) in presCard" :key="index">
             <img :src="card.img" alt="Card Image" />
-            <h3>{{ card.title }}</h3>
-            <p>{{ card.text }}</p>
+            <div class="card-description">
+                <h3>{{ card.title }}</h3>
+                <p>{{ card.text }}</p>
+            </div>
         </div>
     </div>
 </template>
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/var' as *;
+@use '../assets/scss/_mixins' as *;
+
+.card {
+    @include container-md;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    width: calc(100% / 2);
+    padding: 10px;
+
+    h1 {
+        padding: 50px 0;
+    }
+
+    img {
+        background-color: $c-r;
+        padding: 10px;
+        border-radius: 50%;
+    }
+}
+
+.card-description {
+
+    padding-left: 50px;
+}
+</style>
