e4c336530d5056b910dbaac7f9331cbc880b5dbb
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 16:25:55 +0200

    presentation card first draft

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -12,9 +12,9 @@ export default {
 }
 </script>
 <template>
+    <h1>{{ title }}</h1>
     <div class="card">
-        <h1>{{ title }}</h1>
-        <div v-for="(card, index) in presCard" :key="index">
+        <div class="card-container" v-for="(card, index) in presCard" :key="index">
             <img :src="card.img" alt="Card Image" />
             <div class="card-description">
                 <h3>{{ card.title }}</h3>
@@ -27,27 +27,42 @@ export default {
 @use '../assets/scss/var' as *;
 @use '../assets/scss/_mixins' as *;
 
+h1 {
+    @include container-md;
+    padding-top: 50px;
+    padding-bottom: 30px;
+}
+
 .card {
     @include container-md;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     flex-wrap: wrap;
-    width: calc(100% / 2);
-    padding: 10px;
+    width: calc(180% / 2);
+    padding-bottom: 50px;
 
-    h1 {
-        padding: 50px 0;
-    }
+
 
     img {
         background-color: $c-r;
         padding: 10px;
         border-radius: 50%;
     }
+
+    p {
+        padding-top: 10px;
+    }
+}
+
+.card-container {
+    display: flex;
+    width: calc(100% / 2);
+    align-items: flex-start;
+    padding: 30px 100px;
 }
 
 .card-description {
-
-    padding-left: 50px;
+    padding: 0 30px;
 }
 </style>
