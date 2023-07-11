21265e6295c9ee325cd56a8fe1678ef27bd636e4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 08:25:33 +0200

    project card bare bones

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,3 +1,22 @@
-<script></script>
-<template></template>
+<script>
+import { ProjectCard } from '../assets/data/data';
+
+export default {
+    data() {
+        return {
+            projectCard: ProjectCard,
+        };
+    },
+};
+</script>
+<template>
+    <div>
+        <div v-for="(card, index) in projectCard" :key="index">
+            <img :src="card.img" alt="Project Image" />
+            <h3>{{ card.title }}</h3>
+            <p>{{ card.category }}</p>
+            <div>{{ card.zoom }}</div>
+        </div>
+    </div>
+</template>
 <script></script>
