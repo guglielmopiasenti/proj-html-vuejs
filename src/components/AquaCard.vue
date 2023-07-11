5fb4ad76b1afe62821315ff608827882804db2e2
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 08:25:14 +0200

    aqua card bare bones

--- src/components/AquaCard.vue
+++ src/components/AquaCard.vue
@@ -1,3 +1,9 @@
 <script></script>
-<template></template>
+<template>
+    <div>
+        <h1>WE HELP TO DELIVER THE BUSINESS CASE</h1>
+        <p>When, while lovely valley teems with vapour around meand meridian sun strikes the upper impenetrable foliage of
+            my trees, and but a thousand unknown</p>
+    </div>
+</template>
 <script></script>
