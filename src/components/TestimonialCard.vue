1ba176de1fc569d29966d8ad25e8f47d86e7aebc
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-11 08:25:44 +0200

    testimonial bare bones

---
+++ src/components/TestimonialCard.vue
@@ -0,0 +1,21 @@
+<script>
+import { TestimonialCard } from '../assets/data/data';
+
+export default {
+    data() {
+        return {
+            testimonialCard: TestimonialCard,
+        };
+    },
+};
+</script>
+<template>
+    <div>
+        <div v-for="(card, index) in testimonialCard" :key="index" class="testimonial-card">
+            <p>{{ card.text }}</p>
+            <p>{{ card.author }}</p>
+            <p>{{ card.company }}</p>
+            <img :src="card.img" alt="Testimonial Image" />
+        </div>
+    </div>
+</template>
