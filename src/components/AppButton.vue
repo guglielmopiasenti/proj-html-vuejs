2c13563d6fb46ba1fcc8415968944e9b6d3e7cd0
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 20:45:35 +0200

    creating button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'Btn',
+    props: {
+        button: String
+    }
+};
+</script>
+<template>
+    <button class="btn">
+        {{ button }}
+    </button>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/var' as *;
+
+.btn {
+    font-family: Raleway;
+    position: relative;
+    background-color: $c-a;
+    border: none;
+    letter-spacing: 4px;
+    color: $c-g3;
+    text-transform: uppercase;
+    font-weight: 400;
+    padding: 20px 35px;
+    font-size: 16px;
+    transition: color 0.5s ease;
+
+    &:hover {
+        color: white;
+        background-color: $c-r;
+    }
+}
+</style>
