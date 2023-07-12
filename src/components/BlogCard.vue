a354fa4bcb97a385ccffc77ad93500ebc057b7a4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 13:14:22 +0200

    blogcard first draft

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -10,17 +10,50 @@ export default {
 };
 </script>
 <template>
-    <template>
-        <div>
-            <div v-for="(card, index) in blogCard" :key="index" class="blog-card">
-                <img :src="card.img" alt="Blog Image" />
+    <div class="blog-container">
+        <div v-for="(card, index) in blogCard" :key="index" class="blog-card">
+            <div class="blog-img" :style="{ backgroundImage: `url(${card.img})` }"></div>
+            <div class="blog-content">
                 <p>{{ card.date }}</p>
                 <p>{{ card.author }}</p>
                 <h3>{{ card.title }}</h3>
                 <p>{{ card.text }}</p>
             </div>
         </div>
-    </template>
+    </div>
 </template>
-<script>
-</script>
+
+<style scoped lang="scss">
+.blog-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.blog-card {
+    position: relative;
+    width: 600px;
+    height: 400px;
+    margin: 10px;
+
+    .blog-img {
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center;
+    }
+
+    .blog-content {
+        position: absolute;
+        bottom: -25%;
+        right: 0%;
+        width: 80%;
+        height: 50%;
+        background-color: white;
+        padding: 10px;
+        box-sizing: border-box;
+        overflow: auto;
+    }
+}
+</style>
