58a0941ab1ed86515045d9e1de7c891c641351f8
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 17:34:51 +0200

    project card first draft

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -17,20 +17,22 @@ export default {
         <div class="section-container">
             <div>
                 <div>
-                    <div>
+                    <div id="project">
                         PROJECT
                     </div>
-                    <div>{{ title }}</div>
+                    <h1>{{ title }}</h1>
                     <div></div>
                 </div>
             </div>
             <div class="card-components">
-                <div v-for="(card, index) in projectCard" :key="index">
+                <div v-for="(card, index) in projectCard" :key="index" class="card">
                     <img :src="card.img" alt="Project Image" />
-                    <div>
-                        <h3>{{ card.title }}</h3>
-                        <p>{{ card.category }}</p>
-                        <div>{{ card.zoom }}</div>
+                    <div class="overlay">
+                        <div>
+                            <h3>{{ card.title }}</h3>
+                            <p>{{ card.category }}</p>
+                        </div>
+                        <div class="zoom">{{ card.zoom }}</div>
                     </div>
                 </div>
             </div>
@@ -46,7 +48,43 @@ export default {
 }
 
 .section-container {
-    @include container-md
+    @include container-lg;
+    color: white;
+    padding: 180px 0;
+
+    #project {
+        letter-spacing: 1px;
+        position: relative;
+        padding-left: 80px;
+
+        &::after {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 0;
+            width: 60px;
+            height: 3px;
+            background-color: $c-a;
+            transform: translateY(-50%);
+        }
+    }
+
+    h1 {
+        @include second-font;
+        font-size: 40px;
+        width: 60%;
+        padding: 30px 0;
+    }
+
+    h3 {
+        font-weight: 600;
+    }
+
+    .zoom {
+        font-size: 25px;
+        background-color: $c-g3;
+        padding: 10px 20px;
+    }
 }
 
 .card-components {
@@ -55,8 +93,39 @@ export default {
     gap: 20px;
     padding: 30px 0;
 
-    img {
-        width: 350px;
+    .card {
+        position: relative;
+
+        img {
+            width: 400px;
+            transition: all .3s ease-in-out;
+        }
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            display: flex;
+            align-items: flex-end;
+            justify-content: space-between;
+            opacity: 0;
+            transition: opacity .3s ease-in-out;
+            color: white;
+            background: rgba(249, 30, 58, 0.5);
+            padding: 20px;
+        }
+
+        &:hover {
+            img {
+                filter: brightness(.5);
+            }
+
+            .overlay {
+                opacity: 1;
+            }
+        }
     }
 }
 </style>
