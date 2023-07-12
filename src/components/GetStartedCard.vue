0c3f3b36c36b604e7c8cc5ddfc9bfb777344a43e
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 13:34:44 +0200

    get started first draft

--- src/components/GetStartedCard.vue
+++ src/components/GetStartedCard.vue
@@ -1,7 +1,30 @@
 <script>
 export default {
-
+    props: {
+        title: String,
+        par: String,
+    }
 }
 </script>
-<template></template>
-<script></script> 
+<template>
+    <div class="card-container">
+        <h1>{{ title }}</h1>
+        <p>{{ par }}</p>
+    </div>
+</template>
+
+
+<style lang="scss" scoped> .card-container {
+     background-image: url(../assets/img/business-people-working-together-on-project-and-B3MZ4TX.jpg);
+     background-size: cover;
+     background-repeat: no-repeat;
+     padding: 150px 0;
+     margin-top: 200px;
+     text-align: center;
+     color: white;
+
+     h1 {
+         font-size: 30px;
+     }
+ }
+</style> 
