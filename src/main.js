db092416aa7958776e25c656d6c632a913ff695f
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 16:04:46 +0200

    importing fonts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import "@fontsource/bai-jamjuree";
+import "@fontsource/bai-jamjuree/500.css";
+import "@fontsource/bai-jamjuree/600.css";
+import "@fontsource/bai-jamjuree/700.css";
 import "@fontsource/raleway";
+import "@fontsource/raleway/300.css";
+import "@fontsource/raleway/200.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
