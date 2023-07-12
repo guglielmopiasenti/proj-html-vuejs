84885e7bd215a19206c58c239a7b51531d96dd73
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 16:49:27 +0200

    adding line

--- src/components/JumbotronHeader.vue
+++ src/components/JumbotronHeader.vue
@@ -14,13 +14,38 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/mixins' as *;
+@use '../assets/scss/var' as *;
+
 #jumbotron {
-    padding: 200px 0;
+    padding: 250px 0;
     width: 50%;
     color: white;
 }
 
+div {
+    position: relative;
+    font-size: 15px;
+    font-weight: 300;
+    letter-spacing: 4px;
+    padding-left: 90px;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 75px;
+        height: 3px;
+        background-color: $c-a;
+        transform: translateY(-50%);
+    }
+}
+
 h1 {
+    @include second-font;
     font-size: 100px;
+    line-height: 90px;
+    font-weight: 700;
 }
 </style>
