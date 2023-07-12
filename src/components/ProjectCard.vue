384773940d4ead14e3a4a8b4c592d7f0f678b12b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-12 09:23:14 +0200

    first draft project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,6 +2,9 @@
 import { ProjectCard } from '../assets/data/data';
 
 export default {
+    props: {
+        title: String
+    },
     data() {
         return {
             projectCard: ProjectCard,
@@ -10,13 +13,50 @@ export default {
 };
 </script>
 <template>
-    <div>
-        <div v-for="(card, index) in projectCard" :key="index">
-            <img :src="card.img" alt="Project Image" />
-            <h3>{{ card.title }}</h3>
-            <p>{{ card.category }}</p>
-            <div>{{ card.zoom }}</div>
+    <div class="background">
+        <div class="section-container">
+            <div>
+                <div>
+                    <div>
+                        PROJECT
+                    </div>
+                    <div>{{ title }}</div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="card-components">
+                <div v-for="(card, index) in projectCard" :key="index">
+                    <img :src="card.img" alt="Project Image" />
+                    <div>
+                        <h3>{{ card.title }}</h3>
+                        <p>{{ card.category }}</p>
+                        <div>{{ card.zoom }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-<script></script>
+<style lang="scss" scoped >
+@use '../assets/scss/mixins' as *;
+@use '../assets/scss/var' as *;
+
+.background {
+    background-color: $c-g3;
+}
+
+.section-container {
+    @include container-md
+}
+
+.card-components {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+    padding: 30px 0;
+
+    img {
+        width: 350px;
+    }
+}
+</style>
